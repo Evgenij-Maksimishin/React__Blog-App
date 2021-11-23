930e25148657c43fd1fe6d6f3ff69200c3883881
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 23:18:54 +0200

    create BlogData

--- src/BlogPage/BlogPage.js
+++ src/BlogPage/BlogPage.js
@@ -1,28 +1,11 @@
 import React from 'react'
 import '../BlogPage/BlogPage.css'
+import { posts } from '../shared/BlogData'
 
 function BlogPage() {
-    const posts = [
-        {
-            id: 1,
-            title: 'Post 1',
-            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?'
-        },
-        {
-            id: 2,
-            title: 'Post 2',
-            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?'
-        },
-        {
-            id: 3,
-            title: 'Post 3',
-            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?'
-        }
-    ]
-
     const generatePosts = posts.map((item) => {
         return (
-            <div className="post" >
+            <div key={item.id} className="post" >
                 <h2>{item.title}</h2>
                 <p>
                     {item.description}
