37a293f158c506fa7771f08dd0d418f59b5318e0
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 22:04:14 +0200

    create Footer component

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Footer/Footer.css'
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <div>React Blog - 2021</div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
