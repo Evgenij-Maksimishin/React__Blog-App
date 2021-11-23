7446318b1ffdd10d79d73d92126f067be0f3d111
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 21:29:15 +0200

    update header component

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../Header/Header.css'
+
+export default function Header() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <a href="#first">Home</a>
+                    <a href="#second">About</a>
+                    <a href="#third">Contact</a>
+                </nav>
+            </header>
+        </div>
+    )
+}
