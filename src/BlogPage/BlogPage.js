e838860a7d46a37603e580942e6049abb425ad54
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 22:18:43 +0200

    create fn generatePosts

--- src/BlogPage/BlogPage.js
+++ src/BlogPage/BlogPage.js
@@ -20,30 +20,22 @@ function BlogPage() {
         }
     ]
 
+    const generatePosts = posts.map((item) => {
+        return (
+            <div className="post" >
+                <h2>{item.title}</h2>
+                <p>
+                    {item.description}
+                </p>
+            </div>
+        )
+    })
+
     return (
         <>
             <main>
                 <h1>Simple Blog</h1>
-
-                <div className="post" >
-                    <h2>Post 1</h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
-                    </p>
-                </div>
-                <div className="post">
-                    <h2>Post 2</h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
-                    </p>
-                </div>
-                <div className="post">
-                    <h2>Post 3</h2>
-                    <p>
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
-                    </p>
-                </div>
-
+                {generatePosts}
                 <div className="count" style={{ textAlign: "center" }}>
                     <button>Get amount of posts</button>
                 </div>
