9f68080da7b1b6e6e38b3805072915d3fdaa6bae
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 21:43:58 +0200

    create BlogPage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import BlogPage from './BlogPage/BlogPage';
+import Header from './Header/Header'
 
 function App() {
   return (
     <div className="App">
      <h1>fsd</h1>
+     <Header/>
+     <BlogPage/>
     </div>
   );
 }
