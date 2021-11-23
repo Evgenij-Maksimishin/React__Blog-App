d7ac6101112dbc4d5f1fcf11fc8d335402976bb9
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 22:04:52 +0200

    add main tag

--- src/BlogPage/BlogPage.js
+++ src/BlogPage/BlogPage.js
@@ -3,32 +3,34 @@ import '../BlogPage/BlogPage.css'
 
 function BlogPage() {
     return (
-        <div>
-            <h1>Simple Blog</h1>
+        <>
+            <main>
+                <h1>Simple Blog</h1>
 
-            <div className="post" >
-                <h2>Post 1</h2>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
-                </p>
-            </div>
-            <div className="post">
-                <h2>Post 2</h2>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
-                </p>
-            </div>
-            <div className="post">
-                <h2>Post 3</h2>
-                <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
-                </p>
-            </div>
+                <div className="post" >
+                    <h2>Post 1</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
+                    </p>
+                </div>
+                <div className="post">
+                    <h2>Post 2</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
+                    </p>
+                </div>
+                <div className="post">
+                    <h2>Post 3</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?
+                    </p>
+                </div>
 
-            <div className="count" style={{ textAlign: "center" }}>
-                <button>Get amount of posts</button>
-            </div>
-        </div>
+                <div className="count" style={{ textAlign: "center" }}>
+                    <button>Get amount of posts</button>
+                </div>
+            </main>
+        </>
     )
 }
 
