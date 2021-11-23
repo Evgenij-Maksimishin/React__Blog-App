d1e0e895e405e2d357242b9745f41ba425ffc1c6
Yevhenii Maksimishin <evgenijmaksimisin@gmail.com>
2021-11-23 22:13:34 +0200

    create posts array

--- src/BlogPage/BlogPage.js
+++ src/BlogPage/BlogPage.js
@@ -2,6 +2,24 @@ import React from 'react'
 import '../BlogPage/BlogPage.css'
 
 function BlogPage() {
+    const posts = [
+        {
+            id: 1,
+            title: 'Post 1',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?'
+        },
+        {
+            id: 2,
+            title: 'Post 2',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?'
+        },
+        {
+            id: 3,
+            title: 'Post 3',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veritatis libero accusantium omnis saepe, et aliquid! Minus magnam consequuntur obcaecati? Omnis, ratione. Ad dolorum dolorem repudiandae unde, natus nihil maxime, aut eligendi doloribus pariatur aliquid reprehenderit magni a esse assumenda?'
+        }
+    ]
+
     return (
         <>
             <main>
